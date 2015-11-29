d05ff3f99ee04e2e9546a312a3f6713e94e88760
Allison Ng <aln.ng3@gmail.com>
2015-11-29 01:17:24 -0800

    Babby's first jquery/javascript!! Used for showing a section on click for main-menu for icons.

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+// jQuery.fn.extend({
+    // zigzag: function () {
+        // var text = $(this).text();
+        // var zigzagText = '';
+        // var toggle = true; //lower/uppper toggle
+			// $.each(text, function(i, nome) {
+				// zigzagText += (toggle) ? nome.toUpperCase() : nome.toLowerCase();
+				// toggle = (toggle) ? false : true;
+			// });
+	// return zigzagText;
+    // }
+// });
+	jQuery.fn.extend({
+		scrollToBottom: function() {
+		    $('html, body').animate({
+       			 scrollTop: $(this).offset().top
+   			 }, 2000);
+		},
+	});
+
+	$('#button-aboutus').click(function(){
+		$('.main-section').hide();
+		$('#about-us').fadeIn('slow').scrollToBottom();
+	});
+
+	$('#button-contactus').click(function(){
+		$('.main-section').hide();
+		$('#contact-us').fadeIn('slow').scrollToBottom();
+	});
+
+	$('#button-newsletter').click(function(){
+		$('.main-section').hide();
+		$('#newsletter').fadeIn('slow').scrollToBottom();
+	});
+
+	$('#button-openhouses').click(function(){
+		$('.main-section').hide();
+		$('#open-houses').fadeIn('slow').scrollToBottom();
+	});
+
+});
