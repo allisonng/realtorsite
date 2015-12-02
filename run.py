b32356f53b3fdf9a9253ef9322f8f1cbd04ad254
Allison Ng <aln.ng3@gmail.com>
2015-12-02 00:15:22 -0800

    Quickly adding padding for realtyninja embeds and making sure their side margins are auto'd.

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-# import jinja2
-# templateLoader = jinja2.FileSystemLoader(searchpath=".")
-# templateEnv = jinja2.Environment(loader=templateLoader)
-# TEMPLATE_FILE = "template.html"
-# template = templateEnv.get_template(TEMPLATE_FILE)
-# outputText = template.render()
-# print outputText
+import jinja2
+templateLoader = jinja2.FileSystemLoader(searchpath="./templates")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "mls_search.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render()
+print outputText.encode('ascii', 'ignore')
 
-from jinja2 import Environment, FileSystemLoader
-env = Environment(loader=FileSystemLoader)
-template = env.get_template('test.html')
-print(template.render())
+# from jinja2 import Environment, FileSystemLoader
+# env = Environment(loader=FileSystemLoader)
+# template = env.get_template('./templates/test.html')
+# print template.render()
