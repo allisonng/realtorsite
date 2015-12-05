8309dafe55c9c89e344169e951dfaeda7c03daf2
Allison Ng <aln.ng3@gmail.com>
2015-12-04 18:24:07 -0800

    Working on making navigation dynamic (in terms of id="active-page")

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
+# ./templates is where file is held, compiled file will be in same dir as run.py
+# in cmd line: "python run.py > filename.html"
+
 import jinja2
+
+fileList = ["index.html", "current_listings.html", "sold_listings.html", "mls_search.html"]
+fileDict = {'Index': 'index.html', 'Current listings': 'current_listings.html', 'Sold Listings': 'sold_listings.html', 'House Search': 'mls_search.html'}
+
 templateLoader = jinja2.FileSystemLoader(searchpath="./templates")
 templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "mls_search.html"
+# TEMPLATE_FILE = raw_input("Name of file including html: ")
+TEMPLATE_FILE = "index.html"
+# print "User entered: " + TEMPLATE_FILE
 template = templateEnv.get_template(TEMPLATE_FILE)
-outputText = template.render()
+# print template.filename
+outputText = template.render(fileDict=fileDict, fileName=TEMPLATE_FILE)
 print outputText.encode('ascii', 'ignore')
 
 # from jinja2 import Environment, FileSystemLoader
